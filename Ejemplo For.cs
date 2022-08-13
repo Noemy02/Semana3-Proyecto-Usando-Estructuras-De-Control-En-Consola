a183f9e7ced7fd46b58bbc82403d8e742784fc91
Lorena Noemy Bermudez Rodriguez <lorenabermudezro@gmail.com>
2022-08-13 09:35:00 -0600

    Agregación de una tercera clase nueva

---
+++ Ejemplo For.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana3_Proyecto_Usando_Estructuras_De_Control_En_Consola
+{
+    class Ejemplo_For
+    {
+    }
+}
